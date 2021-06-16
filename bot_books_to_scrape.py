ff6a9a9346c1b98b28f182a5810071e6615cb559
gdanj <gdanj21@gmail.com>
2021-06-16 14:39:57 +0200

    création de la fonction allCategoryUrl qui retourne une list avec tout les urls de la categorie de url passé en parametre

--- bot_books_to_scrape.py
+++ bot_books_to_scrape.py
@@ -2,33 +2,38 @@ import requests
 from math import ceil
 from scrapy.selector import Selector
 
-url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
 
 def pageNbr(response):
     """Retourne un Int du nombre de résultats possède cette catégorie,
     en divisant le numbre de livre total par le numbre de vivre par page"""
+
     articlesNbrUrl = "#default > div > div > div > div > form > strong:nth-child(2)" + "::text"
     return ceil(int(Selector(text=response.text).css(articlesNbrUrl).get()) / 20)
 
-def categoriScrap(response):
+
+def scrapAllBooks(response):
     """Retourne une List de Str comtenant les urls de chaque livre."""
 
     urlAllBooks = "div:nth-child(2) > ol > li > article > div.image_container > a::attr(href)"
     return Selector(text=response.text).css(urlAllBooks).getall()
 
+
 def categoriList(response):
     """Retourne une List de Str contenant chaque url des categories"""
+
     urlAllCategory = ".nav > li:nth-child(1) > ul:nth-child(2) > li > a:nth-child(1)::attr(href)"
     return Selector(text=response.text).css(urlAllCategory).getall()
 
+
 def formatUrl(url):
     """Retourne une url valide"""
+
     result = url
     if "../" in url:
         result = url.split("../")[-1]
 
     if "books/" in url:
-        result = "https://books.toscrape.com/catalogue/category/" + result.replace("index.html", "page-1.html")
+        result = "https://books.toscrape.com/"  + result
 
     if "../../" in url:
         result = "https://books.toscrape.com/catalogue/" + result
@@ -36,11 +41,44 @@ def formatUrl(url):
     return result
 
 
+def nestPage(url, totalPageNbr):
+    """Retourne un Str de l'url de la page de résultat suivant, ou un de str vide si il n'y a pas de page suivant"""
 
-response = requests.get(url)
+    if "index.html" in url:
+        url = url.replace("index.html", "page-1.html")
+        currentPageNbr = 1
+    else:
+        currentPageNbr = int(url.split("/")[-1].replace("page-","").replace(".html", ""))
 
-print(pageNbr(response))
-print("\n")
-print(formatUrl(categoriScrap(response)[2]))
-print("\n")
-print(formatUrl(categoriList(response)[2]))
+    if currentPageNbr < totalPageNbr:
+        return url.replace("page-" + str(currentPageNbr) + ".html", "page-" + str(currentPageNbr + 1) + ".html")
+    else:
+        return ""
+
+
+def allCategoryUrl(categoriUrl):
+    """Retourne tout les urls d'une catégorie"""
+
+    currentPageUrl = formatUrl(categoriUrl)
+    response = requests.get(currentPageUrl)
+    totalPageNbr = pageNbr(response)
+    i = 0
+    result = []
+    while i < totalPageNbr:
+        response = requests.get(currentPageUrl)
+        result += scrapAllBooks(response)
+        currentPageUrl = formatUrl(nestPage(categoriUrl, totalPageNbr))
+        i += 1
+    return result
+
+
+def main():
+    url = "https://books.toscrape.com/index.html"
+
+    response = requests.get(url)
+    listCategoryUrl = categoriList(response)
+    for categoriUrl in listCategoryUrl:
+        allCategoryUrl(categoriUrl)
+    
+    
+main()
